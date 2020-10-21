a8266b9006300fa64effe6f32245c6ea04c0e3db
Danny Shi <dannyshi.sr@gmail.com>
2020-10-21 15:15:38 -0400

    changed DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://dannyshi@localhost/jobseek",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://dannyshi@localhost/jobseek",
   CLIENT_ORIGIN:
     process.env.NODE_ENV === "development"
       ? `http://localhost:3000`
