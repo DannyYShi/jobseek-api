bc4f640166ccbb6d3f2ee91678c50012c75bebe9
Danny Shi <dannyshi.sr@gmail.com>
2020-10-22 12:36:14 -0400

    fixed migration script

--- migrations/001.do.create_jobseek.sql
+++ migrations/001.do.create_jobseek.sql
@@ -5,15 +5,6 @@ CREATE TABLE lists
 	list_name text not null
 );
 
-INSERT INTO lists
-    (list_name)
-VALUES
-    ('Wishlist'),
-    ('Applied'),
-    ('Interview'),
-    ('Offer'),
-    ('Rejected');
-
 CREATE TABLE cards
 (
     list_id INTEGER REFERENCES lists(list_id),
@@ -24,12 +15,4 @@ CREATE TABLE cards
     job_url TEXT,
     job_description TEXT
 );
-
-INSERT INTO cards
-    (list_id, company_name, position_applied, job_location, job_url, job_description)
-VALUES
-    (1, 'Apple', 'Genius', 'Cupertino', 'www.apple.com/careers', 'This is an example'),
-    (1, 'Uber', 'Driver', 'San Francisco', 'www.uber.com/careers', 'This is another example'),
-    (2, 'Google', 'Googler', 'Atlanta', 'www.google.com/careers', 'This is an example in another list');
-
 
